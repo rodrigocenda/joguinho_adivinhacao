507ce5b4524dd79829fc4ca1d70520783cf1dfe9
Rodrigo Cendamore Ceccon <119449931+rodrigocenda@users.noreply.github.com>
2023-06-27 16:29:25 -0300

    Update jogo adivinhação.py
    Código um pouco mais organizado em funções

--- jogo adivinhação.py
+++ jogo adivinhação.py
@@ -1,39 +1,51 @@
 import random
 
-numero=(random.randrange(1,101))
-rodada=1
-pontos=1000
-print("Qual o nível de dificuldade?")
-print("(1) Fácil (2) Médio (3) Difícil")
-
-nivel = int(input("Defina o nível: "))
-
-if (nivel == 1):
-    tentativas=15
-elif (nivel == 2):
-    tentativas=10
-else:
-    tentativas=5
-
-
-for rodada in range(1, tentativas+1):
-    print("Tentativa {} de {}".format(rodada, tentativas))
-    chute=int(input("adivinhe o número de 1-100: "))
-    if chute>100 or chute<0:
-        print("de 1-100 só")
-        continue
-    else:
-        if (chute==numero):
-            print("voce acertou \n pontos: {} \n número: {} \n\n parabens".format(pontos, numero))
-            break
+def jogar():
+    
+    inicio() 
+    numero=(random.randrange(1,101))
+    rodada=1
+    pontos=1000
+    tentativas=dificuldade()
+    
+    for rodada in range(1, tentativas+1):
+        print("Tentativa {} de {}".format(rodada, tentativas))
+        chute=int(input("adivinhe o número de 1-100: "))
+        if chute>100 or chute<0:
+            print("de 1-100 só")
+            continue
         else:
-            if chute<numero:
-                print("maior")
-                if (rodada == tentativas):
-                    print("O número secreto era {}. Você fez {} pontos".format(numero, pontos))
+            if (chute==numero):
+                print("voce acertou \n pontos: {} \n número: {} \n\n parabens".format(pontos, numero))
+                break
             else:
-                print('menor')
-                if (rodada == tentativas):
-                    print("O número secreto era {}. Você fez {} pontos".format(numero, pontos))
-            pontos_perdidos=abs(numero-chute)
-            pontos=pontos-pontos_perdidos
+                if chute<numero:
+                    print("maior")
+                    if (rodada == tentativas):
+                        print("O número secreto era {}. Você fez {} pontos".format(numero, pontos))
+                else:
+                    print('menor')
+                    if (rodada == tentativas):
+                        print("O número secreto era {}. Você fez {} pontos".format(numero, pontos))
+                pontos_perdidos=abs(numero-chute)
+                pontos=pontos-pontos_perdidos
+                
+def inicio():
+    print("***********************************************")
+    print("       Bem vindo ao jogo de adivinhação!       ")
+    print("***********************************************")
+    print("Qual o nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")                
+
+def dificuldade():
+    nivel=int(input("Defina o nível: "))
+    if (nivel == 1):
+        tentativas=15
+    elif (nivel == 2):
+        tentativas=10
+    else:
+        tentativas=5
+    return tentativas
+            
+if __name__== "__main__":
+    jogar()
